8b938cd7912ffba0adc1da104622e318054ee938
Z1nnlos <Noel.Sabia@gmx.net>
2023-12-11 19:44:40 +0100

    safety push

--- src/sort_helper2.c
+++ src/sort_helper2.c
@@ -6,7 +6,7 @@
 /*   By: nsabia <nsabia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 15:40:11 by nsabia            #+#    #+#             */
-/*   Updated: 2023/12/11 19:18:34 by nsabia           ###   ########.fr       */
+/*   Updated: 2023/12/11 19:41:25 by nsabia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,23 +31,29 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	return (dst);
 }
 
+// int	find_spot_in_b(int number, int *stack_len_b, int *stack_b)
+// {
+// 	int	len_b;
+// 	int	i;
+
+// 	len_b = *stack_len_b;
+// 	i = 1;
+// 	while (i < len_b)
+// 	{
+// 		if (number < stack_b[i - 1] && number > stack_b[i])
+// 			return (i);
+// 		i++;
+// 	}
+// 	if (number < stack_b[len_b - 1])
+// 		return (len_b);
+// 	return (0);
+// }
+
 int	find_spot_in_b(int number, int *stack_len_b, int *stack_b)
 {
-	int	len_b;
-	int	i;
-
-	len_b = *stack_len_b;
-	i = 1;
-	while (i < len_b)
-	{
-		if (number < stack_b[i - 1] && number > stack_b[i])
-			return (i);
-		i++;
-	}
-	if (number < stack_b[len_b - 1])
-		return (len_b);
-	return (0);
+	
 }
+
 // the problem seems to be different.
 // It will execute rbb or rb until you spot > 0 is false. 
 // It cannot find a good place in the stack for that case
@@ -56,3 +62,4 @@ int	find_spot_in_b(int number, int *stack_len_b, int *stack_b)
 // ALSO check if stack_b[*stack_len_b] is bigger 
 // than the number i want to push
 
+//Taskverse
